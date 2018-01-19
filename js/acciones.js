02028fa3a65802aefe8d8a62f7e37666608a265d
soujiro27 <jc.raus27@gmail.com>
2018-01-18 23:21:22 -0600

    validaciones completas

--- js/acciones.js
+++ js/acciones.js
@@ -11,8 +11,8 @@ module.exports = class Acciones {
 		$('form#Acciones').submit(function(e) {
 			e.preventDefault()
 			let datos = $(this).serializeArray()
-			//let validacion = base.valida_letras(datos)
-			let validacion = base.valida_length(datos,[3])
+			let validacion = base.valida_string_complete(datos,[3])
+			//let validacion = base.valida_length(datos,[3])
 			console.log(validacion)
 
 		})
--- js/base.js
+++ js/base.js
@@ -22,24 +22,67 @@ module.exports = class Base {
 		return lista
 	}
 
-	valida_letras(datos,max) {
-		var resultados = new Array()
+
+	valida_string_complete(datos,max){
+		let resultados = new Array()
+		
+		resultados.push(this.valida_empty(datos))
+		//resultados.push(this.valida_letras(datos))
+		//resultados.push(this.valida_length(datos,max))
+		return resultados
+	}
+
+	valida_letras(datos) {
+		let resultados = new Array()
 		$.each(datos, function(index, val) {
 			 if(!validator.isAlpha(datos[index].value)){
 			 	resultados.push({
 			 		campo:datos[index].name,
-			 		message:'EL campos solo puede contener Letras'
+			 		message:'El campo solo puede contener Letras'
 			 	})
 			 } 
 
-			 if(validator.isLength(datos[index].value,{min:1,max:max[index]})){
-				 resultados.push({
-			 		campo:datos[index].name,
-			 		message:'EL campos solo puede contener Letras'
-			 	})	
-			 }
+			
 		});
 		return resultados
 	}
+
+	valida_length(datos){
+		let resultados = new Array()
+		$.each(datos,function(index,el){
+			 if(!validator.isLength(datos[index].value,{min:1,max:max[index]})){
+				 resultados.push({
+			 		campo:datos[index].name,
+			 		message:`El campo NO puede tener mas de ${max} caracteres`
+			 	})	
+			 }
+		})
+		return resultados
+	}
 	
+	valida_empty(datos) {
+		let resultados = new Array()
+		$.each(datos,function(index,el){
+			if(validator.isEmpty(datos[index].value)){
+				 resultados.push({
+			 		campo:datos[index].name,
+			 		message:'El campos NO puede estar vacio'
+			 	})	
+			}
+		})
+		return resultados
+	}
+
+	valida_numeros(datos) {
+		let resultados = new Array()
+		$.each(datos,function(index,el){
+			if(validator.isNumeric(datos[index].value)){
+				resultados.push({
+			 		campo:datos[index].name,
+			 		message:'El campos solo puede contener numeros'
+			 	})	
+			}
+		})
+		return resultados
+	}
 }
